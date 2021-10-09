12f387e59360cab7c637a0699c0fdcf6cc740bc2
norply <75418405+norply@users.noreply.github.com>
2021-10-09 23:56:13 +0500

    Начало
    Подключаем библиотеки для ввода/вывода, использования русского языка и строк. Создаём алфавит в виде строки.

---
+++ Cesar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+#include <stdlib.h>
+#include <string>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    setlocale(LC_CTYPE, "rus");
+	string text;//для  введённых данных
+    int n;// Шаг сдвига
+    int size;// Размер  введённой строки
+	string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+}
